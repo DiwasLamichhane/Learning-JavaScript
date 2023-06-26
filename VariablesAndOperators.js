c000ffa07b612a6ab7b48b9a5ba478c1c6cbfdc0
Diwas Lamichhane <104636668+DiwasLamichhane@users.noreply.github.com>
2023-06-26 20:49:23 +0545

    Variables And Operators
    Creating Variables and Operators In JS.

---
+++ VariablesAndOperators.js
@@ -0,0 +1,54 @@
+// Creating variables
+
+        // var a = 22; // This is integer 
+        // var b = "DIWAS";  //This is a string 
+        // var c = 33.55;
+        // console.log(b) // This will print b (i.e. DIWAS) 
+
+// Operators in JavaScript
+        
+        // In 3+4 '+' is the operator and 3, 4 are the operands
+        // Operand: Entities on which operator operates
+
+  // Types:
+
+  // 1) Unary Operator
+
+        // // It has single operand (x=-x)
+        // // example:
+        // var c= 69.69;
+        // c=-c;
+        // console.log(c)
+
+  // 2) Binary Operator
+
+        // // It has two opeand (x=x+6)
+        // // example:
+        // var c = 65.66;
+        // c= c+2;
+        // console.log(c)
+
+        // Arithmetic operators in action in JS
+
+        var num1 = 4;
+        var num2 = 2;
+
+        console.log("The value of num1 + num2 is " + (num1+num2))
+        console.log("The value of num1 - num2 is " + (num1-num2))
+        console.log("The value of num1 * num2 is " + (num1*num2))
+        console.log("The value of num1 / num2 is " + (num1/num2))
+        
+ // Exponentiation Operator (**)
+        console.log("The value of num1 ** num2 is " + (num1**num2)) // power i.e. num1^num2
+
+// Increment (++) and Decrement operator (--)
+
+        // console.log("The value of num1++ is " + (num1++)) // Increment num1 by 1 (Result will come num1 only because num1++ will first print num1 and then do increment. so if num1 is 4 then result will come 4 only)
+
+        console.log("The value of ++num1 is " + (++num1)) // Increment num1 by 1 (first num1 will increament and then print)
+
+        // Decrement Operator pani same as increment operator
+
+        // console.log("The value of num1-- is " + (num1--))
+        console.log("The value of --num1 is " + (--num1))
+        
