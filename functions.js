049f6d485bf2bdb99aa555469125a854c24b3f86
Diwas Lamichhane <104636668+DiwasLamichhane@users.noreply.github.com>
2023-06-27 18:56:17 +0545

    Functions in JavaScript
    Learning about functions in javascript.

---
+++ functions.js
@@ -0,0 +1,18 @@
+console.log("This is function tutorial");
+
+function greet (name) {
+    console.log(greetText + name);
+    console.log(name + " is a good boy.")    
+}
+let greetText = "Good Morning ";
+let name = "Diwas";
+let name1 = "Aaryan";
+let name2 = "Manjil";
+let name3 = "Mishek";
+let name4 = "Pradeepta";
+
+greet(name, greetText);
+greet(name1, greetText);
+greet(name2, greetText);
+greet(name3, greetText);
+greet(name4, greetText);
