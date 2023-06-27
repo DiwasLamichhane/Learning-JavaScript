c5976598a5317114b09d45f10c02c725a8648415
Diwas Lamichhane <104636668+DiwasLamichhane@users.noreply.github.com>
2023-06-27 18:42:20 +0545

    String & String Methods
    String & String Methods.

---
+++ string.js
@@ -0,0 +1,16 @@
+var string = 'This is "String"'
+        var name = 'Diwas'
+        var message = ' Codes Well'
+        var temp = `${name} Is Learning Coding And He${message}` //${...} works like fill in the blanks.
+        var len = name.length
+
+
+        console.log(temp);
+        console.log(name+message);
+        console.log(`The length of name is ${len}`) //this will print the length of name (name count garxa jastai name variable ma diwas xa diwas ma 5 ota letter xa so 5 print hunxa)
+
+        // escape sequence characters
+
+        console.log("Hello\nWorld") // \n represents a new line
+        var y = new String("this");
+        console.log(y);
