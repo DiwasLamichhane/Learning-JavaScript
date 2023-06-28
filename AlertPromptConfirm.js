191bb21e2122e9748f26b3707b548ee579537565
Diwas Lamichhane <104636668+DiwasLamichhane@users.noreply.github.com>
2023-06-28 20:48:38 +0545

    Alert, Prompt & Confirm
    Interaction using alert, prompt and confirm in Js.

---
+++ AlertPromptConfirm.js
@@ -0,0 +1,21 @@
+   // Alert 
+        // alert("This is a pirated site!");
+
+        // Prompt 
+        // let Name = prompt("Enter your name.", "Guest");
+        // console.log(Name);
+
+        // Confirm 
+        // let confirmation = confirm("Are you sure you want to close this tab ?");
+        // console.log(confirmation);
+
+
+        // Creating 18+ confirmation on webpage.
+        // let age = 18;
+        let userAge = prompt("Enter your age.");
+        if (userAge>=18){
+            alert("You are eligible.");
+        }
+        else{
+            alert("You are not eligible.");
+        }
