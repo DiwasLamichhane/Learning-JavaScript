f4054ed3c070f4404ee8ee78ae1d17fddfdf923d
Diwas Lamichhane <104636668+DiwasLamichhane@users.noreply.github.com>
2023-06-28 20:53:10 +0545

    Manipulating DOM

---
+++ ManipulatingDOM.js
@@ -0,0 +1,18 @@
+
+        // get element by id 
+
+        let main = document.getElementById('main');
+        console.log(main);
+
+        // get element by class 
+
+        let containers = document.getElementsByClassName('container');
+        console.log(containers);
+
+        // selector 
+
+        // let selector0 = document.querySelector('.container');
+        // console.log('Selector Returns', selector0);
+
+        let selector1 = document.querySelectorAll('.container');
+        console.log('Selector Returns', selector1);
