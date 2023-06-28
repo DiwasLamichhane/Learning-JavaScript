c0204360f0e6ae32c0d7e041497ca61bdf664276
Diwas Lamichhane <104636668+DiwasLamichhane@users.noreply.github.com>
2023-06-28 20:49:47 +0545

    Loops In Js
    Learning about loops in js

---
+++ loops.js
@@ -0,0 +1,51 @@
+        // For Loop
+
+
+        // let i=0;
+        // for(i=0; i<3; i++){
+        // console.log(i);
+        // }
+
+        
+        // ForEach Loop
+
+
+        let friends = ["Diwas", "Nikita", "Aayush", "Dishant", "Binayak"];
+        // for (let index = 0; index < friends.length; index++) {                                                         
+        //     console.log("Hello Friend " + friends[index]);
+        // }
+
+        // friends.forEach(function f(element){
+        //     console.log("Hello Friend " + element + " to Modern JavaScript.");
+        // });
+
+
+        // ForOF Loop
+
+
+        // for (element of friends) {
+        //     console.log("Hello Friend " + element + " to Modern JavaScript again.");    
+        // }
+
+
+        // ForIn Loop "use this loop to iterate over objects in javascript"
+
+
+        // let student ={
+        //     name: "Diwas",
+        //     class: "bachelor 2nd year",
+        //     subject: "Bsc. Csit"
+        // }
+
+        // for (key in student){
+        //     console.log(`The ${key} of student is ${student[key]}`);
+        // }
+
+
+        // While loop
+
+        let i=0;
+        while(i<4){
+            console.log(`${i} is less than 4.`);
+            i++;
+        }
